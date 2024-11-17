2395b18532653bb29adf051e319fb52deb8c780e
isaacrabs <119681538+isaacrabs@users.noreply.github.com>
2024-11-17 13:49:48 +0000

    getting somewhere

--- block-blast.py
+++ block-blast.py
@@ -8,10 +8,12 @@ class Grid:
             self.grid.append(' '*8)
 
     def display_grid(self):
+        count = 1
+        print("    1   2   3   4   5   6   7   8 ")
         for item in self.grid:
-            print(" ___ ___ ___ ___ ___ ___ ___ ___")
-            print(f"|{item[0]}|{item[1]}|{item[2]}|{item[3]}|{item[4]}|{item[5]}|{item[6]}|{item[7]}|")
-
+            print("   ___ ___ ___ ___ ___ ___ ___ ___")
+            print(f"{count} |{item[0]}|{item[1]}|{item[2]}|{item[3]}|{item[4]}|{item[5]}|{item[6]}|{item[7]}| {count}")
+        print("    1   2   3   4   5   6   7   8 ")
 
 
 
@@ -20,18 +22,27 @@ class Game:
 
     def __init__(self):
         self.grid = Grid()
-        self.allowedBlocks = []
+        self.allowedBlocks = {}
         self.score = 0
+        self.possibleMoves = True
 
     def display_score(self):
         print("\n")
         print(f"Your score is: {self.score} ")
+        print("\n")
 
     def turn(self):
+        self.display_score()
         self.grid.display_grid()
 
-    def setup(self):
+
+
+    def game(self):
         self.grid.create_grid()
+        while self.possibleMoves:
+
+
+
 
 
 class Blocks:
